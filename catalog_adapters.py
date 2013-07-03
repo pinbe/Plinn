56a307ecc4a84fab4a72ce037fe39ad20fbc16f7
Benoît Pin <benoit.pin@gmail.com>
2013-07-03 23:49:45 +0200

    bugfix : on vérifie que le parent implémente bien l'interface d'ordonancement.

--- catalog_adapters.py
+++ catalog_adapters.py
@@ -1,7 +1,9 @@
 from Products.CMFCore.CatalogTool import IndexableObjectWrapper
+from OFS.interfaces import IOrderedContainer
 
 class PlinnIndexableObjectWrapper(IndexableObjectWrapper) :
     def position(self) :
         parent = self.getParentNode()
-        pos = parent.getObjectPosition(self.getId())
-        return pos
+        if IOrderedContainer.providedBy(parent) :
+            pos = parent.getObjectPosition(self.getId())
+            return pos
