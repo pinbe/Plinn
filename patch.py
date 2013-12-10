dacc4a8732118811d10ecff1cee34e6c920769c3
Benoît Pin <pin@cri.ensmp.fr>
2013-12-10 11:28:06 +0100

    Ajout de la méthode put_upload. Config sécurité.

--- patch.py
+++ patch.py
@@ -3,10 +3,26 @@
 
 
 """
+from AccessControl import ClassSecurityInfo
+from Products.CMFCore.permissions import ListFolderContents, View, ViewManagementScreens,\
+                                         ManageProperties, AddPortalFolders, AddPortalContent,\
+                                         ManagePortal, ModifyPortalContent
+
 from Products.CMFCore.PortalFolder import PortalFolder as CMFPortalFolder
 from Folder import PlinnFolder
 
+cmfPortalFolderSecurity = ClassSecurityInfo()
+
+cmfPortalFolderSecurity.declareProtected(ListFolderContents, 'listFolderContents')
 CMFPortalFolder.listFolderContents = PlinnFolder.listFolderContents.im_func
+
+cmfPortalFolderSecurity.declareProtected(ListFolderContents, 'listNearestFolderContents')
 CMFPortalFolder.listNearestFolderContents = PlinnFolder.listNearestFolderContents.im_func
+
+cmfPortalFolderSecurity.declareProtected(ListFolderContents, 'listCatalogedContents')
 CMFPortalFolder.listCatalogedContents = PlinnFolder.listCatalogedContents.im_func
+
+cmfPortalFolderSecurity.declareProtected(AddPortalContent, 'put_upload')
 CMFPortalFolder.put_upload = PlinnFolder.put_upload.im_func
+
+cmfPortalFolderSecurity.apply(CMFPortalFolder)
