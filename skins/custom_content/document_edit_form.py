0f2a8853f243d533e0348c043c3e42175dcb17a5
Benoît Pin <benoit.pin@gmail.com>
2014-04-09 16:45:38 +0200

    On s'assure que le dossier des pièces jointes existe.

--- skins/custom_content/document_edit_form.py
+++ skins/custom_content/document_edit_form.py
@@ -1,25 +1,28 @@
 ##parameters=change='', change_and_view='', ajax=''
-##
+from Products.CMFCore.utils import getToolByName
+atool = getToolByName(context, 'portal_attachment')
+attachments = atool.getAttachmentsFor(context)
+
 form = context.REQUEST.form
 text = form.get('text')
 if text and same_type(text, []) :
-	# when javascript is disabled,
-	# there's a hidden textarea from epoz
-	# and an other from <noscript> tag
-	form.update({'text' : text[1]}) 
+    # when javascript is disabled,
+    # there's a hidden textarea from epoz
+    # and an other from <noscript> tag
+    form.update({'text' : text[1]}) 
 
 if change and \
-		context.validateTextFile(**form) and \
-		context.validateHTML(**form) and \
-		context.document_edit_control(text=form.get('text'), text_format='html') and \
-		context.setRedirect(context, 'object/edit', **{'ajax':ajax}):
-	return
+        context.validateTextFile(**form) and \
+        context.validateHTML(**form) and \
+        context.document_edit_control(text=form.get('text'), text_format='html') and \
+        context.setRedirect(context, 'object/edit', **{'ajax':ajax}):
+    return
 elif change_and_view and \
-		context.validateTextFile(**form) and \
-		context.validateHTML(**form) and \
-		context.document_edit_control(text=form.get('text'), text_format='html') and \
-		context.setRedirect(context, 'object/view', **{'ajax':ajax}):
-	return
+        context.validateTextFile(**form) and \
+        context.validateHTML(**form) and \
+        context.document_edit_control(text=form.get('text'), text_format='html') and \
+        context.setRedirect(context, 'object/view', **{'ajax':ajax}):
+    return
 
 
 options = {}
@@ -29,6 +32,6 @@ target = context.getActionInfo('object/edit')['url']
 buttons.append( {'name': 'change', 'value': 'Change'} )
 buttons.append( {'name': 'change_and_view', 'value': 'Change and View'} )
 options['form'] = { 'action': target,
-					'listButtonInfos': tuple(buttons) }
+                    'listButtonInfos': tuple(buttons) }
 
 return context.document_edit_template(**options)
