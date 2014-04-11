3d59c472dd284fff830f11a5683b4148bf4b4f24
Benoît Pin <pin@cri.ensmp.fr>
2014-04-11 10:35:46 +0200

    Retrait des éléments notés deprecated depuis mathusalem. Jslint.

--- skins/ajax_scripts/fragment_importer.js
+++ skins/ajax_scripts/fragment_importer.js
@@ -10,13 +10,10 @@ var FragmentImporter;
 
 var isTextMime = /^text\/.+/i;
 
-FragmentImporter = function(url, onAfterPopulate, baseElement) {
+FragmentImporter = function(url, onAfterPopulate) {
 	var thisImporter = this;
 	this.url = url;
 	this.onAfterPopulate = (!onAfterPopulate) ? function(){return;} : onAfterPopulate;
-	this.baseElement = baseElement;
-	if (baseElement && window.console)
-		console.warn('Deprecation warning : usage of baseElement will be removed. Use an xml based response.');
 };
 
 FragmentImporter.prototype._load = function(url) {
@@ -40,12 +37,12 @@ FragmentImporter.prototype._load = function(url) {
 				break;
 			case 4 :
 				hideProgressImage();
-				if (req.status == '200')
-					thisImporter.populateBaseElement(req);
-				else
-					alert('Error: ' + req.status);
-				
-		};
+				if (req.status === 200) {
+					thisImporter.populateBaseElement(req); }
+				else {
+					alert('Error: ' + req.status); }
+				break;
+		}
 	};
 
 	req.open("GET", url, true);
@@ -53,17 +50,17 @@ FragmentImporter.prototype._load = function(url) {
 };
 
 FragmentImporter.prototype.load = function(fallBackUrl) {
-	if (fallBackUrl)
-		this._fallBackUrl = fallBackUrl;
-	else
-		this._fallBackUrl = this.url;
+	if (fallBackUrl) {
+		this._fallBackUrl = fallBackUrl; }
+	else {
+		this._fallBackUrl = this.url; }
 	this._load(this.url);
 };
 
 FragmentImporter.prototype.useMacro = function(template, macro, fragmentId, queryString) {
 	var url = this.url + "/use_macro?template=" + template + "&macro=" + macro + "&fragmentId=" + fragmentId;
-	if (queryString)
-		url += '&' + queryString;
+	if (queryString) {
+		url += '&' + queryString; }
 	this._load(url);
 };
 
@@ -76,38 +73,23 @@ FragmentImporter.prototype.populateBaseElement = function(req) {
 		window.location.href = this._fallBackUrl;
 		return;
 	}
-	if (contentType.indexOf('text/xml') != -1) {
+	if (contentType.indexOf('text/xml') !== -1) {
 		var fragments = req.responseXML.documentElement.childNodes;
-		var fragment, dest, scripts;
-		for (var i=0 ; i < fragments.length ; i++) {
+		var fragment, dest, scripts, i, j;
+		for (i=0 ; i < fragments.length ; i++) {
 			fragment = fragments[i];
-			if (fragment.nodeName == 'fragment') {
+			if (fragment.nodeName === 'fragment') {
 				dest = document.getElementById(fragment.getAttribute('id'));
-				if(!dest)
-					continue;
-				dest.innerHTML = fragment.firstChild.nodeValue;
-				
-				scripts = dest.getElementsByTagName('script');
-				for (var j=0 ; j < scripts.length ; j++)
-					globalScriptRegistry.loadScript(scripts[j]);
+				if(dest) {
+					dest.innerHTML = fragment.firstChild.nodeValue;
+					scripts = dest.getElementsByTagName('script');
+					for (j=0 ; j < scripts.length ; j++) {
+						globalScriptRegistry.loadScript(scripts[j]); }
+				}
 			}
 		}
 	}
-	else {
-		this.baseElement.innerHTML = req.responseText;
-	
-		var scripts = this.baseElement.getElementsByTagName('script');
-		for (var i=0 ; i < scripts.length ; i++)
-			globalScriptRegistry.loadScript(scripts[i]);
-	}
-
-	var onAfterPopulate = this.onAfterPopulate;
-	if (typeof(onAfterPopulate) == "string") {
-		if (console)
-			console.warn('Deprecation warning : onAfterPopulate may not be a string (' + onAfterPopulate + ')');
-		onAfterPopulate = eval(onAfterPopulate);
-	}
-	onAfterPopulate();
+	this.onAfterPopulate();
 };
 
-})();
+}());
