c15d698f84d77c34393d33dbe492d44694619010
Benoît Pin <pin@cri.ensmp.fr>
2014-09-17 15:05:01 +0200

    Préférences linguistiques enregistrées dans les données utilisateur et utilisées en priorité (par rapport au préférences du navigateur).

---
+++ userlanguages.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from zope.publisher.browser import BrowserLanguages
+from Products.CMFCore.utils import getUtilityByInterfaceName
+
+class AuthenticatedUserLanguages(BrowserLanguages):
+
+    def getPreferredLanguages(self) :
+        mtool = getUtilityByInterfaceName('Products.CMFCore.interfaces.IMembershipTool')
+        if mtool.isAnonymousUser() :
+            return super(AuthenticatedUserLanguages, self).getPreferredLanguages()
+        else :
+            m = mtool.getAuthenticatedMember()
+            userLangs = m.getProperty('preferred_languages', [])
+            return userLangs or super(AuthenticatedUserLanguages, self).getPreferredLanguages()
