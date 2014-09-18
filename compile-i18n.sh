fdb6f4e9ae1d1933e56c76b141fac14ac235960d
Benoît Pin <pin@cri.ensmp.fr>
2014-09-18 14:42:17 +0200

    Mise à jour (msgmerge) du fichier de localisation de CMF avec la version actuelle.

--- compile-i18n.sh
+++ compile-i18n.sh
@@ -3,5 +3,5 @@
 msgfmt -o locales/fr/LC_MESSAGES/plinn.mo locales/fr/LC_MESSAGES/plinn.po
 msgfmt -o locales/en/LC_MESSAGES/plinn.mo locales/en/LC_MESSAGES/plinn.po
 
-
+msgfmt -o locales/fr/LC_MESSAGES/cmf_default.mo locales/fr/LC_MESSAGES/cmf_default.po
 msgfmt -o locales/en/LC_MESSAGES/cmf_default.mo locales/en/LC_MESSAGES/cmf_default.po
