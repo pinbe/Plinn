c49b6c19c96514ff27166c337314a31ef4e414aa
Benoît Pin <pin@cri.ensmp.fr>
2014-09-29 10:52:47 +0200

    autorisation des tags form, input et textarea.

--- __init__.py
+++ __init__.py
@@ -98,7 +98,12 @@ validTags = default_cmf_utils.NASTY_TAGS.copy()
 for tag in validTags.keys() :
     validTags[tag] = 1
 
-VALID_TAGS = {'font': 1, 'param' : 1, 'iframe' : 1}
+VALID_TAGS = {'font': 1,
+              'param' : 1,
+              'iframe' : 1,
+              'form' : 1,
+              'input' : 1,
+              'textarea' : 1}
 
 validTags.update(VALID_TAGS)
 
