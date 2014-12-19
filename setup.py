037bd31f6ba4844d1efa9a303d2adfcf433c8fe3
Benoît Pin <pin@cri.ensmp.fr>
2014-12-19 16:09:18 +0100

    métadonnées++

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ from setuptools import setup, find_packages
 setup(name='Products.Plinn',
       version='3.0',
       descripton='Plinn content management framework',
-      author="Benoît Pin – MINES ParisTech – Armines"
+      url='http://plinn.org',
+      author="Benoît Pin – MINES ParisTech – Armines",
+      author_email="benoit.pin@mines-paristech.fr",
       license="GPL",
       packages=find_packages(),
       include_package_data=True,
       namespace_packages=['Products'],
-      zip_safe=False
+      zip_safe=False,
+      install_requires=[] #TODO
       )
