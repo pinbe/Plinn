786c9a860f1354f83ff198ebae4b10a871fffb73
Benoît Pin <pin@cri.ensmp.fr>
2017-11-16 01:37:14 +0100

    fix pour Safari

--- Products/Plinn/skins/flex/js/layereddocument.js
+++ Products/Plinn/skins/flex/js/layereddocument.js
@@ -1,8 +1,8 @@
 (function() {
     window.addEventListener('load', function() {
         var fixedContent = document.getElementById('fixed-content');
-        window.addEventListener('scroll', function(evt) {
-            fixedContent.style.top = - (evt.pageY/2) + 'px';
+        window.addEventListener('scroll', function() {
+            fixedContent.style.top = - (window.scrollY / 2) + 'px';
         });
 
     });
