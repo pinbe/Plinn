74c9b4f8621baa998225b9e319e5a44db88c5dfd
Benoît Pin <pin@cri.ensmp.fr>
2017-12-11 19:30:34 +0100

    Ajout d'un sous onglet d'édition pour paramétrer la cinématique du layered document.

--- Products/Plinn/skins/content/layered_document_edit_form.py
+++ Products/Plinn/skins/content/layered_document_edit_form.py
@@ -1,4 +1,4 @@
-##parameters=save='', layer=None, ajax=''
+##parameters=save='', layer=None, edit_type='layer', ajax=''
 from Products.CMFCore.utils import getToolByName
 from ZTUtils import make_query as mq
 from Products.Plinn.utils import Message as _
@@ -17,11 +17,16 @@ if save and \
 options = {}
 target = context.getActionInfo('object/edit')['url']
 options['form'] = {'action' : target}
+options['edit_type'] = edit_type
 
 layers_length = context.getLayerLength()
 
 options['selected_layer'] = layers_length - 1 if layer is None else layer
-options['selected_tab'] = 'layer_%d' % options['selected_layer']
+if edit_type == 'layer' :
+    options['selected_tab'] = 'layer_%d' % options['selected_layer']
+else :
+    options['selected_tab'] = edit_type
+
 options['layer_html'] = context.getLayer(options['selected_layer'])
 
 options['layerstabs'] = []
@@ -34,15 +39,14 @@ for ln in range(layers_length, 0, -1) :
         title = _('Layer ${n}', mapping={'n' : ln})
 
     options['layerstabs'].append(
-            {'available' : True,
-             'category' : 'object',
-             'description' : '',
-             'icon' : '',
-             'title' : title,
-             'url' : '%s?%s' % (target, mq(layer=ln-1)),
-             'visible' : True,
-             'allowed' : True,
-             'link_target' : None,
+            {'title' : title,
+             'url' : '%s?%s' % (target, mq(edit_type='layer',layer=ln - 1)),
              'id' : 'layer_%d' % (ln - 1)})
+
+options['layerstabs'].append(
+        {'title' : _('Kinematic'),
+         'url' : '%s?%s' % (target, mq(edit_type='kinematic')),
+         'id' : 'kinematic'}
+)
 
 return context.layered_document_edit_template(**options)
