a3d7b159a04e7ff5808c5d2014df2d4a1b324169
Benoît Pin <pin@cri.ensmp.fr>
2017-12-11 14:38:57 +0100

    La mise en forme du à la con pour IE6 retirée.

--- Products/Plinn/skins/generic/group_actions.py
+++ Products/Plinn/skins/generic/group_actions.py
@@ -3,14 +3,14 @@
 portal_url = context.portal_url()
 
 return [{'id' : 'groups_members',
-         'name' : "Manage group's members",
+         'title' : "Manage group's members",
          'url' : portal_url + '/groups_members'},         
         {'id' : 'group_data',
-         'name' : "Manage group's datas",
+         'title' : "Manage group's datas",
          'url' : portal_url + '/group_data'},
         {'id' : 'portal_members',
-         'name' : "Manage portal's members",
+         'title' : "Manage portal's members",
          'url' : portal_url + '/portal_members'},
         {'id' : 'portal_all_groups',
-         'name' : "Manage portal's groups",
+         'title' : "Manage portal's groups",
          'url' : portal_url + '/portal_all_groups'}]
