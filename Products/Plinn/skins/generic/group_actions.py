673b8c19de28807d843b736ba3d3a608ddaf29d3
Benoît Pin <pin@cri.ensmp.fr>
2017-12-11 15:36:09 +0100

    Ordonnancement les onglets de l'annuaire.

--- Products/Plinn/skins/generic/group_actions.py
+++ Products/Plinn/skins/generic/group_actions.py
@@ -1,16 +1,17 @@
 ##parameters=
 
 portal_url = context.portal_url()
-
-return [{'id' : 'groups_members',
-         'title' : "Manage group's members",
-         'url' : portal_url + '/groups_members'},         
-        {'id' : 'group_data',
-         'title' : "Manage group's datas",
-         'url' : portal_url + '/group_data'},
-        {'id' : 'portal_members',
-         'title' : "Manage portal's members",
-         'url' : portal_url + '/portal_members'},
-        {'id' : 'portal_all_groups',
-         'title' : "Manage portal's groups",
-         'url' : portal_url + '/portal_all_groups'}]
+actions = [
+    {'id' : 'portal_members',
+     'title' : "Manage portal's members",
+     'url' : portal_url + '/portal_members'},
+    {'id' : 'groups_members',
+     'title' : "Manage group's members",
+     'url' : portal_url + '/groups_members'},
+    {'id' : 'group_data',
+     'title' : "Manage group's datas",
+     'url' : portal_url + '/group_data'},
+    {'id' : 'portal_all_groups',
+     'title' : "Manage portal's groups",
+     'url' : portal_url + '/portal_all_groups'}]
+return actions
