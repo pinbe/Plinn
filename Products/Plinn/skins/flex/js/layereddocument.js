a02508a6d2ab4fcff624ea25091745578b15e16d
Benoît Pin <pin@cri.ensmp.fr>
2017-12-12 16:42:41 +0100

    JSHint

--- Products/Plinn/skins/flex/js/layereddocument.js
+++ Products/Plinn/skins/flex/js/layereddocument.js
@@ -2,7 +2,6 @@
     window.addEventListener('load', function() {
         var fixedContent = document.getElementById('fixed-content');
         var options = JSON.parse(fixedContent.firstElementChild.getAttribute('data-options'));
-        console.log(options);
 
         var stitchAtterElt = (options.stitch_after) ?
             document.querySelector(options.stitch_after) :
@@ -18,7 +17,7 @@
 
 
         var speedup = (options.bg_scroll_speedup) ? parseInt(options.bg_scroll_speedup) : 0.5;
-        speedup = (speedup === NaN) ? 0.5 : speedup;
+        speedup = (isNaN(speedup)) ? 0.5 : speedup;
 
         window.addEventListener('scroll', function() {
             fixedContent.style.top = topOffset() - (window.scrollY * speedup) + 'px';
