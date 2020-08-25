81323320ff6a50f93e1f89f972be2ec7ab1069c5
Benoît Pin <pin@cri.ensmp.fr>
2020-08-25 13:39:28 +0200

    Allé, va-t-on s'en sortir avec l'optimisation des imports ?

--- Products/Plinn/utils.py
+++ Products/Plinn/utils.py
@@ -24,7 +24,7 @@
 
 import re
 import string
-from json import dumps as json_dumps, json_loads
+from json import dumps as json_dumps, loads as json_loads
 from quopri import encodestring
 from random import randrange
 
